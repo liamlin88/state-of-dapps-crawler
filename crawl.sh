8a1c4bb23d5c7c2841b5aff0885e24e52b76bbb9
Zhitao Lin <zhitao@narf.sail.cs.queensu.ca>
2019-06-18 09:02:35 -0400

    Date update - 18-06-2019

--- crawl.sh
+++ crawl.sh
@@ -9,3 +9,7 @@ DATE=$(date +%d-%m-%Y)
 scrapy crawl mainpage_promote -t csv -o - >"data/promote/$DATE-mainpage_promote.csv"
 scrapy crawl features -t csv -o - >"data/feature/$DATE-feature.csv"
 scrapy crawl dapps -t csv -o - >"data/dapp/$DATE-dapp.csv"
+
+git add -A
+git commit -m "Date update - $DATE"
+git push
